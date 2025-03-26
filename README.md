55bdfadea0cc8184196101bf86caa9e0d8c46388
ashx3s <ashlyn@lilyx.io>
2025-03-26 09:51:37 -0600

    unsafe code version

--- README.md
+++ README.md
@@ -6,6 +6,32 @@
 - What makes sql injection a common vulnerability? why is it dangerous?
 - What are common pitfalls developers face when implementing validation?
 
+## Tasks
+
+- Client side validations for UX:
+  -Name: Only allow alphabetic characters and limit the length to 50 characters.
+  - Email: Use a regular expression to ensure the input follows standard email formatting (e.g., username@domain.com).
+  - Phone: Validate to allow only numeric characters and limit to a specific format, such as (123) 456-7890.
+  - Message: Restrict input to plain text, disallowing HTML tags and special characters.
+- Security Policy
+  - html loaded (not recommended)
+  ```
+   <meta
+      http-equiv="Content-Security-Policy"
+      content="default-src 'self'; script-src 'self' https://trusted-cdn.com;"
+    />
+  ```
+
+### Resources
+
+- [client side form validation](https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Form_validation)
+- [js form validation](
+  Name: Only allow alphabetic characters and limit the length to 50 characters.
+  Email: Use a regular expression to ensure the input follows standard email formatting (e.g., username@domain.com).
+  Phone: Validate to allow only numeric characters and limit to a specific format, such as (123) 456-7890.
+  Message: Restrict input to plain text, disallowing HTML tags and special characters.
+  )
+
 ## Input Validation Quick Notes
 
 - [OWASP Input Validation Cheat Sheet](https://cheatsheetseries.owasp.org/cheatsheets/Input_Validation_Cheat_Sheet.html)
@@ -13,6 +39,8 @@
 - Semantic (enforce correct values for their context)
 - Client side (validation done at the point where users input content -- can be circumvented with web proxys): this improves UX more
 - Server side (validation on the server before the data is processed by the application): this improves security more
+- [Validity.valid](https://developer.mozilla.org/en-US/docs/Web/API/ValidityState) used with html inputs
+- [Form Validation](https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Form_validation)
 
 ## Class Discussion
 
--- public/script.js
+++ public/script.js
@@ -1,18 +1,38 @@
 // Function to handle form submission and display a thank you message
+function validateField(el) {
+  if (!el.validity.valid) {
+    console.error("Input is invalid!", el.id, el.validationMessage);
+    console.log(el);
+    return false;
+  } else {
+    console.log("Input is valid", el.id);
+    return true;
+  }
+}
 function submitForm() {
   // Get user inputs
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const message = document.getElementById("message").value;
-
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const message = document.getElementById("message");
+  /*
+  if (
+    !validateField(name) ||
+    !validateField(email) ||
+    !validateField(phone) ||
+    !validateField(message)
+  ) {
+    console.error("AAH NOT VALIDATED");
+    return;
+  }
+    */
   // Display Thank You message with user details
   const thankYouMessage = `<h3>We've Got Mail!</h3>
-                                    <p>Thank you, ${name}! Our team will be in touch soon — stay tuned!</p>
-                                     <p><strong>Name:</strong> ${name}</p>
-                                     <p><strong>Email:</strong> ${email}</p>
-                                     <p><strong>Phone:</strong> ${phone}</p>
-                                     <p><strong>Message:</strong> ${message}</p>`;
+                                    <p>Thank you, ${name.value}! Our team will be in touch soon — stay tuned!</p>
+                                     <p><strong>Name:</strong> ${name.value}</p>
+                                     <p><strong>Email:</strong> ${email.value}</p>
+                                     <p><strong>Phone:</strong> ${phone.value}</p>
+                                     <p><strong>Message:</strong> ${message.value}</p>`;
   document.getElementById("thankYouMessage").innerHTML = thankYouMessage;
   document.getElementById("thankYouMessage").style.display = "block";
 
